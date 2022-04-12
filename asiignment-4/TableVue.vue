72fd571fba11391ef568f13b24fee1bb046c047b
ramarao-500apps <ramarao.taddi@mantra.com>
2022-04-12 19:33:17 +0530

    commit

---
+++ asiignment-4/TableVue.vue
@@ -0,0 +1,99 @@
+<template>
+
+<div>
+
+<h1>Table view</h1>
+
+<table align="center">
+
+<thead>
+
+<tr>
+
+<th>email</th>
+
+<th>id</th>
+
+<th>name</th>
+
+</tr>
+
+</thead>
+
+<tr v-for="array in array" :key="array.id">
+
+<td>{{ array.email }}</td>
+
+<td>{{ array.id }}</td>
+
+<td>{{ array.name }}</td>
+
+</tr>
+
+<tbody>
+
+</tbody>
+
+</table>
+
+</div>
+
+</template>
+
+<script>
+
+export default {
+
+name: "TablevieW2",
+
+data() {
+
+return {
+
+array: [
+
+{email: "Eliseo@gardner.biz", id: 1, name: "id labore ex et quamlaborum"},
+
+{email: "Jayne_Kuhic@sydney.com", id: 2, name: "quo vero reiciendisvelit similique earum"},
+
+{email: "Nikita@garfield.biz", id: 3, name: "odio adipisci rerumaut animi"},
+
+{email: "Lew@alysha.tv", id: 4, name: "alias odio sit"}
+
+],
+
+};
+
+},
+
+};
+
+</script>
+
+<style scoped>
+
+table {
+
+font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+
+border-collapse: collapse;
+
+width: 45%;
+
+background-color: white;
+
+}
+
+td,
+
+th {
+
+border: 5px solid #dddddd;
+
+text-align: left;
+
+padding: 10px;
+
+}
+
+</style>
---
+++ asiignment-4/loginpage.vue
@@ -0,0 +1,56 @@
+<template>
+
+<div>
+
+<h1>logindetails</h1>
+
+<form name="myform" method="post" onsubmit="fun()" >
+
+Enter Your Name: <input type="text" name="name" placeholder="Enter FirstName" required><br/> <br>
+
+Enter Your Email:<input type="email" name="Email" placeholder="Enter Email" required><br/> <br>
+
+Enter Your Age: <input type="number" name="Age" placeholder="Enter Age" required><br/> <br>
+
+Enter Your ContactNo: <input type="number" name="Contact" placeholder="Enter ContactNo" required><br/>
+
+<button @click="fun()">Save
+
+</button>
+
+</form>
+
+</div>
+
+</template>
+
+<script>
+
+export default {
+
+name:"LOgin",
+
+data()
+
+{
+
+return {
+
+}
+
+},
+
+methods:{
+
+fun(){
+
+document.write("welcome"+" "+document.myform.name.value+"</br>Your MailID: "+document.myform.Email.value+"</br> Your Age: "+document.myform.Age.value+"</br> Your Contact Number"+document.myform.Contact.value);
+
+}
+
+}
+
+}
+
+</script>
+
