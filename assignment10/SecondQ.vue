8055d7f3e7d7bc25306e66de47e265ad7f3d2b7e
ramarao-500apps <ramarao.taddi@mantra.com>
2022-04-27 18:40:34 +0530

    26-4-2022

---
+++ assignment10/SecondQ.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>SecondQ</h1>
+    <b-form-input
+      type="Number"
+      id="multiplication"
+      placeholder="Enter a Firstnumber"
+    ></b-form-input>
+    <b-form-input
+      type="Number"
+      id="divison"
+      placeholder="Enter a Secondnumber "
+    ></b-form-input>
+    <b-button @click="f1()">Multiply</b-button><br /><br />
+    <b-button @click="f2()">Divided</b-button>
+    <p id="calculation"></p>
+  </div>
+</template>
+<script>
+export default {
+  name: "SecondQ",
+  data() {
+    return {
+      multiplication: [],
+      divison: [],
+    };
+  },
+  methods: {
+    f1() {
+      document.getElementById("calculation").innerHTML =
+        document.getElementById("multiplication").value *
+        document.getElementById("divison").value;
+      return this.multiplication;
+    },
+    f2() {
+      document.getElementById("calculation").innerHTML =
+        document.getElementById("multiplication").value /
+        document.getElementById("divison").value;
+      return this.divison;
+    },
+  },
+};
+</script>
